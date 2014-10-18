097ea2e46ade053ac076d2fef1f0df896f81015d
JanEltner <j.eltner@gmx.de>
2014-10-18 23:40:13 +0200

    Update infinitPoint.java
    Added the needed functionality to the infinit point

--- ellipticCurve/src/ellipticCurve/infinitPoint.java
+++ ellipticCurve/src/ellipticCurve/infinitPoint.java
@@ -1,5 +1,8 @@
 package ellipticCurve;
 
+import java.math.BigInteger;
+
+
 public class infinitPoint implements PointInterface{
 
 	@Override
@@ -11,5 +14,23 @@ public class infinitPoint implements PointInterface{
 	public boolean isInfinityPoint()
 	{
 		return true;
+	}
+
+	@Override
+	public PointInterface add(PointInterface point2) 
+	{
+		return point2;
+	}
+
+	@Override
+	public PointInterface doublePoint() 
+	{
+		return this;
+	}
+
+	@Override
+	public PointInterface multiply(BigInteger factor) 
+	{
+		return this;
 	}
 }
