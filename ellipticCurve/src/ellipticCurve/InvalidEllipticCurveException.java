4f310ccaeb008c7e8d7c9b2f7d7ca3ecce30ec83
JanEltner <j.eltner@gmx.de>
2014-10-18 23:46:51 +0200

    Update InvalidEllipticCurveException.java
    Added explanation why the elliptic curve is invalid

--- ellipticCurve/src/ellipticCurve/InvalidEllipticCurveException.java
+++ ellipticCurve/src/ellipticCurve/InvalidEllipticCurveException.java
@@ -3,5 +3,9 @@ package ellipticCurve;
 public class InvalidEllipticCurveException extends Exception {
 
 	private static final long serialVersionUID = 6296681455618667516L;
+	public InvalidEllipticCurveException() 
+	{
+		super("Elliptic curves need to fulfil the equation 4*a^3+ 27*b^2 mod p != 0");
+	}
 
 }
