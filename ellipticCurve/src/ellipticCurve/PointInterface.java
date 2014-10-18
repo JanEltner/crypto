dc5ef712b763174bb13e121706ae961353c58d73
JanEltner <j.eltner@gmx.de>
2014-10-18 23:41:34 +0200

    Update PointInterface.java
    Encapsulated more funktions to the PointInterface

--- ellipticCurve/src/ellipticCurve/PointInterface.java
+++ ellipticCurve/src/ellipticCurve/PointInterface.java
@@ -1,6 +1,11 @@
 package ellipticCurve;
 
+import java.math.BigInteger;
+
 public interface PointInterface 
 {
 	public boolean isInfinityPoint();
+	public PointInterface add(PointInterface point2);
+	public PointInterface doublePoint();
+	public PointInterface multiply(BigInteger factor);
 }
