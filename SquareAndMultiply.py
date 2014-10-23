ded01bd6f4a466ac9827db8a959aa27c5a362946
JanEltner <j.eltner@gmx.de>
2014-10-23 21:13:40 +0200

    Update SquareAndMultiply.py

--- SquareAndMultiply.py
+++ SquareAndMultiply.py
@@ -1,15 +1,9 @@
-def exp(base, exponent):
-	binExp = []
-	count = 0
-	result = 1
-	if exponent > 0:
-		result*=base
-	while(exponent > 0):
-		binExp.append(exponent%2)
-		exponent = exponent>>1
-		count += 1
-	for i in range(count-2,-1,-1):
-		result *= result
-		if binExp[i] == 1:
-			result *= base
+def exp(base, exponent,prime):
+	result = base
+	binExp=bin(exponent)[3:]
+	for i in binExp:
+		result = result * result
+		if(i):
+			result = base * result
+		result = result % prim
 	return result
