e9b905d90e33b5eb8c083537743c3ad69631db94
JanEltner <j.eltner@gmx.de>
2015-01-06 00:11:15 +0100

    use lambda and a small improvement in the output
    use a lamda to calculate the remainder
    label the columns in the output

--- extendedEuclideanAlgorithm.py
+++ extendedEuclideanAlgorithm.py
@@ -2,11 +2,13 @@ def calcMultiplicativeInverse(factor, modulo):
 	a,b = factor,modulo
 	u,t = 1,1
 	v,s = 0,0
+	r = lambda x,y:(int(x-(q*y)))
+	print("a | b | s | v | u | t")
 	while(b > 0):
 		print("{0} | {1} | {2} | {3} | {4} | {5}".format(b,a,s,v,u,t))
 		q,a,b = int((a-a%b)/b),b,a%b
-		u,s = s,int(u - (q*s))
-		v,t = t,int(v - (q*t))
+		u,s = s,r(u,s)
+		v,t = t,r(v,t)
 	print("multiplicative inverses: {:d}".format(u%modulo))
 	print("gcd({:d},{:d}) = {:d}".format(factor,modulo,a))
 	print("{:d} = {:d}*{:d} + ({:d})*{:d}".format(a,u,factor,v,modulo))
