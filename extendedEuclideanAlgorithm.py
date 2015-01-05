d5ffbd1674ed2f89ec0fb2ff6d90441380db7ed7
JanEltner <j.eltner@gmx.de>
2015-01-05 23:59:19 +0100

    Cleanup
    - avoided tmp variables
    - use "".format() instad of "" + str() + ""
    - everything a bit shorter ;)

--- extendedEuclideanAlgorithm.py
+++ extendedEuclideanAlgorithm.py
@@ -1,24 +1,14 @@
 def calcMultiplicativeInverse(factor, modulo):
-	a = factor
-	b = modulo
-	u = 1
-	t = 1
-	v = 0
-	s = 0
+	a,b = factor,modulo
+	u,t = 1,1
+	v,s = 0,0
 	while(b > 0):
 		print("{0} | {1} | {2} | {3} | {4} | {5}".format(b,a,s,v,u,t))
-		tmp = a%b
-		q = int((a-tmp)/b)
-		a = b
-		b = tmp
-		tmp = int(u - (q*s))
-		u = s
-		s = tmp
-		tmp = int(v - (q*t))
-		v = t
-		t = tmp
-	print("multiplicative inverses: " +str(u%modulo))
-	print("gcd("+str(factor)+","+str(modulo) + ") = " + str(a))
-	print(str(a) + " = " + str(u) + "*" + str(factor) + " + (" + str(v) + ")*" + str(modulo))
+		q,a,b = int((a-a%b)/b),b,a%b
+		u,s = s,int(u - (q*s))
+		v,t = t,int(v - (q*t))
+	print("multiplicative inverses: {:d}".format(u%modulo))
+	print("gcd({:d},{:d}) = {:d}".format(factor,modulo,a))
+	print("{:d} = {:d}*{:d} + ({:d})*{:d}".format(a,u,factor,v,modulo))
 	return u
 calcMultiplicativeInverse(5, 7)
